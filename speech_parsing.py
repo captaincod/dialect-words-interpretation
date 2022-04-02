2f0b826990a4b5cf3cdad30ed26312c34a6bbc95
Olesya Ivanova <iod5000@gmail.com>
2022-04-02 11:01:26 +0800

    speech parsing and lemmatization

---
+++ speech_parsing.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from tqdm import tqdm
+
+stories_dir = Path("funny_stories")
+for path in tqdm(sorted(stories_dir.glob("*"))):
+    file = open(path, encoding='utf-8')
+    new_path = str(stories_dir) + '\CLEAR_' + str(path)[len(str(stories_dir)) + 1:]
+    new_f = open(new_path, 'w', encoding='utf-8')
+
+    tag_signs = ['&lt;', '&gt;']
+    inside_tag = False
+    one_bytes_signs = ['-', '!', '.', ',', '?', ':']
+    two_bytes_signs = ['¡']
+
+    word = ""
+    text = ""
+
+    for line in file:
+        for i in range(len(line)):
+            ch = line[i]
+            byte_char = ch.encode('utf-8')
+
+            if ch == " " or ch in one_bytes_signs:
+                if word != "" and not inside_tag:
+                    text += word + ch
+                word = ""
+
+            elif ch == "-" and line[i+1].isalpha():
+                text += word + '-'
+                word = ""
+
+            elif ch == ';':
+                if line[i-3:i+1] == tag_signs[0]:
+                    inside_tag = True
+                elif line[i-3:i+1] == tag_signs[1]:
+                    inside_tag = False
+
+            elif len(byte_char) == 2:
+                if ch not in two_bytes_signs:
+                    word += ch
+
+
+    file.close()
+    new_f.write(text)
+    new_f.close()
+
